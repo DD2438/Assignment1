5b7d57011137f9c482501dc4cb4345fdcb7a1758
Arash-s <arash859@hotmail.com>
2016-01-26 18:10:09 +0100

    Create CsvReader.java

---
+++ src/CsvReader.java
@@ -0,0 +1,42 @@
+package Astar;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class CsvReader {
+
+	String path;
+	int row, col;
+	
+	public CsvReader(String path, int row, int col){
+		this.path = path;
+		this.row = row;
+		this.col = col;
+	}
+	
+	public boolean[][] convert(){
+		boolean[][] map = new boolean[col][row];
+		
+		String line = "";
+		String cvsSplitBy = ",";
+		try {
+			
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			int counter=0;
+			
+			while ((line = br.readLine()) != null) {
+
+			        // use comma as separator
+				String[] vals = line.split(cvsSplitBy);
+
+				for(int i=0; i<col; i++){
+					if(Integer.valueOf(vals[i])==1)
+						map[i][counter] = true;
+				}
+				counter++;
+			}
+
+		} catch(Exception e){}
+		return map;
+	}
+}
